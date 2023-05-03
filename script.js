89cae7b92717ba013a5157ce96932f89c948f5ab
dirtTW <31981880+yichifauzi@users.noreply.github.com>
2023-05-04 01:43:18 +0700

    rework the back to top btn

--- script.js
+++ script.js
@@ -24,3 +24,28 @@ function showAndHide(clickedButton) {
     }
   }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the button
+  let topbutton = document.getElementById("topBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topbutton.style.display = "block";
+    } else {
+      topbutton.style.display = "none";
+    }
+  }
+
+  // Add event listener to the button
+  topbutton.addEventListener("click", topFunction);
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+});
