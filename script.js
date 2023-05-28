2e6fd15f5f7e970329c8f2506176dd2465101419
dirtTW <31981880+yichifauzi@users.noreply.github.com>
2023-05-29 02:06:38 +0700

    Update some component

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-function showAndHide(clickedButton) {
-  var mpinstall = document.getElementById("mpinstall");
-  var pltutorial = document.getElementById("pltutorial");
-
-  if (clickedButton === "mpinstall") {
-    if (mpinstall.classList.contains("hide")) {
-      mpinstall.classList.remove("hide");
-      mpinstall.classList.add("show");
-      pltutorial.classList.remove("show");
-      pltutorial.classList.add("hide");
-    } else {
-      mpinstall.classList.remove("show");
-      mpinstall.classList.add("hide");
-    }
-  } else if (clickedButton === "pltutorial") {
-    if (pltutorial.classList.contains("hide")) {
-      pltutorial.classList.remove("hide");
-      pltutorial.classList.add("show");
-      mpinstall.classList.remove("show");
-      mpinstall.classList.add("hide");
-    } else {
-      pltutorial.classList.remove("show");
-      pltutorial.classList.add("hide");
-    }
-  }
-}
-
 document.addEventListener("DOMContentLoaded", function () {
   // Get the button
   let topbutton = document.getElementById("topBtn");
